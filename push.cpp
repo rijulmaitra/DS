5ca8065909c33c570c7e2e192a04dedb6bc82771
rijulmaitra <rijulmaitra@gmail.com>
2025-05-12 01:30:16 +0530

    Initial commit: added stack implementation

---
+++ push.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Stack{
+    private:
+        int top;
+        int a[10];
+        int value;
+    public:
+        Stack(){
+            top = -1;
+        }
+        void input(){
+            cout << "Enter element: " ;
+            cin >> value;
+        }
+        void push(){
+            if (top >= 9){
+                cout << "Stack overflow! \n";
+                return; 
+            }
+            a[++top] = value;
+            cout<< "Pushed " << value << " into Stack. \n"; 
+        }
+        void display(){
+            if (top == -1){
+                cout << "Stack is empty \n";
+                return; 
+            }
+        }
+
+};
+int main(){
+    Stack s;
+    s.input();
+    s.push();
+    s.display();
+    return 0;
+}
