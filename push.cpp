91bcd7c4b6583cda142acbdd95f739a80f746c16
rijulmaitra <rijulmaitra@gmail.com>
2025-05-12 02:03:49 +0530

    Updated stack logic

--- push.cpp
+++ push.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
 using namespace std;
-class Stack{
-    private:
-        int top;
-        int a[10];
-        int value;
-    public:
-        // Declaring Constructor
-        Stack(){
-            top = -1;
+
+class Stack {
+private:
+    int top;
+    int a[10];
+    int value;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    void input() {
+        cout << "Enter element: ";
+        cin >> value;
+    }
+
+    void push() {
+        if (top >= 9) {
+            cout << "Stack overflow!\n";
+            return;
         }
-        void input(){
-            cout << "Enter element: " ;
-            cin >> value;
+        a[++top] = value;
+        cout << "Pushed " << value << " into Stack.\n";
+    }
+
+    void display() {
+        if (top == -1) {
+            cout << "Stack is empty\n";
+            return;
         }
-        void push(){
-            if (top >= 9){
-                cout << "Stack overflow! \n";
-                return; 
-            }
-            a[++top] = value;
-            cout<< "Pushed " << value << " into Stack. \n"; 
+        cout << "Stack elements: ";
+        for (int i = 0; i <= top; i++) {
+            cout << a[i] << " ";
         }
-        void display(){
-            if (top == -1){
-                cout << "Stack is empty \n";
-                return; 
-            }
-        }
+        cout << "\n";
+    }
+
+    bool isFull() {
+        return top >= 9;
+    }
 };
-int main(){
+
+int main() {
     Stack s;
-    for (int i=0; i<=10; i++){
+    for (int i = 0; i < 13; i++) {
         s.input();
         s.push();
         s.display();
+        if (s.isFull()) {
+            break;
+        }
     }
-    return 0;    
+    return 0;
 }
