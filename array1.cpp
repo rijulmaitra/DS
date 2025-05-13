045dd000172ff0ff4d9006c6ebf8cabcb44c611e
rijulmaitra <rijulmaitra@gmail.com>
2025-05-13 11:59:37 +0530

    Update array1.cpp

--- array1.cpp
+++ array1.cpp
@@ -1,13 +1,24 @@
 #include <iostream> 
 using namespace std; 
 int main() {
-  
     int a[4]={1,2,3,4};
+    int b[]={3,5,7,9};
+    cout << "----------------" << endl;
+    // display all elements
     for (int i=0; i<4; i++){
         cout << a[i] << endl;
     }
+    cout << "----------------" << endl;
     // size of arrray
     cout << sizeof(a) << endl;
+    cout << "----------------" << endl;
+    // address of elements
+    cout << &a[0] << endl;
+    cout << &a[1] << endl;
+    cout << &a[2] << endl;
+    cout << &a[3] << endl;
+    cout << "----------------" << endl;
+    
 
   return 0;
 }
