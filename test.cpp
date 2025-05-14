8b706141f485f7ee0668c1e7eb3f6ee7479594d1
rijulmaitra <rijulmaitra@gmail.com>
2025-05-14 20:28:55 +0530

    Update

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// HIGHEST NUMBER IN ARRAY
+
+// int main(){
+//     int a[]={1,3,10,9,7};
+//     int target = a[0];
+//     for(int i=0; i<5; i++){
+//         if(a[i]>target){
+//             target=a[i];
+//         }
+//     }
+//     cout <<target<<endl;
+//     return 0;    
+// }
+
+
+
+
