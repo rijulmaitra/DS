ebbf7dd0c5cde13e4e0e8dca926efc0e824987ef
rijulmaitra <rijulmaitra@gmail.com>
2025-05-14 15:20:05 +0530

    Update linearSearch.cpp

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,25 +1,44 @@
 // Linear Search in Array
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int linear_search(int arr[], int n, int target) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == target) {
-            return i;
-        }
-    }
-    return -1;
-}
+class LinearSearch{
+    public:
+        void Linear(){
+            int a[]={24,56,34,69,12};
+            int n=sizeof(a)/sizeof(a[0]);
+            bool flag=false;
+            int target;
+            cout<< "Enter the number you want to search: ";
+            cin>> target;
 
+            for (int i = 0; i < n; i++){
+                if (a[i]==target){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==true){
+                cout<<target<<" is present in the array!"<<endl;
+            }
+            else{
+                cout<<target<<" number not found."<<endl;
+            }
+        }
+};
 int main() {
-    int arr[] = {2, 4, 0, 1, 9};
-    int target = 1;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = linear_search(arr, n, target);
-    if (result != -1) {
-        cout << "Element found at index " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
+    LinearSearch l;
+    string answer;
+    while (true)
+    {
+        l.Linear();
+        cout << "Do you want to exit?(yes/no): ";
+        cin >>answer;
+        transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+        if(answer=="yes"){
+            break;
+        }
     }
     return 0;
 }
