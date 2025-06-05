7223b5e51db201fc62855dc56c2214588769aaac
rijulmaitra <rijulmaitra@gmail.com>
2025-06-05 11:31:45 +0530

    Update singleLinkedList.cpp

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -7,8 +7,6 @@ struct Node{
     Node(int val){
         data = val;
         next = NULL;
-
-
     }
 };
 
@@ -22,14 +20,34 @@ class List{
         }
         void push_front(int val){
             Node* newNode=new Node(val);
-            Node newNode(val);
+            // Node newNode(val);
+            
+            if (head == NULL){
+                head = tail = newNode;
+                return;
+            }else{
+                newNode ->next = head;
+                head=newNode;
+            }
 
+        }
+
+        void display(){
+            Node* temp = head;
+            while(temp!=NULL){
+                cout << temp->data << "\n";
+                temp = temp->next;
+            }
         }
 
 };
 
 int main() {
     List l;
-    
+    l.push_front(1);
+    l.push_front(2);
+    l.push_front(3);
+    l.display();
+
     return 0;
 }
