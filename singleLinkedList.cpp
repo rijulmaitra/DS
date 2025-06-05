6863cb24c2950a82c8c0cfb7862ac261ac412c18
rijulmaitra <rijulmaitra@gmail.com>
2025-06-05 12:05:18 +0530

    Update

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -32,12 +32,52 @@ class List{
 
         }
 
+        void push_back(int val){
+            Node* newNode=new Node(val);
+            if (head==NULL){
+                head=tail=newNode;
+            }else{
+                tail->next=newNode;
+                tail=newNode;
+            }
+        }
+
+        void pop_front(){
+            if(head==NULL){
+                cout<<"1st element is null" << endl;
+                return;
+            }else{
+                Node* temp = head;
+                head=head->next;
+                temp->next=NULL;
+                delete temp;
+            }
+        }
+
+        void pop_back(){
+            if(head==NULL){
+                cout<<"Last element is null" << endl;
+                return;
+            }else{
+                Node* temp = head;
+                while (temp->next!=tail)
+                {
+                    
+                    temp=temp->next;
+                }
+                temp->next=NULL;
+                delete tail;
+                tail=temp;
+            }
+        }
+
         void display(){
             Node* temp = head;
             while(temp!=NULL){
-                cout << temp->data << "\n";
+                cout << temp->data << "->";
                 temp = temp->next;
             }
+            cout<<"NULL"<<endl;
         }
 
 };
@@ -47,7 +87,12 @@ int main() {
     l.push_front(1);
     l.push_front(2);
     l.push_front(3);
+    l.push_back(7);
     l.display();
-
+    l.pop_front();
+    l.display();
+    l.pop_back();
+    l.display();
+    
     return 0;
 }
